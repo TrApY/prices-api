d9b9aaf7f87b50197c3f78a2c84a465dfe42764c
Alex Marín Serna <13417217+TrApY@users.noreply.github.com>
2026-08-20 09:10:36 +0200

    fix: registrar la excepción inesperada antes de responder 500

--- src/main/java/com/inditex/prices/infrastructure/adapter/in/rest/ApiExceptionHandler.java
+++ src/main/java/com/inditex/prices/infrastructure/adapter/in/rest/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.inditex.prices.infrastructure.adapter.in.rest;
 
 import com.inditex.prices.domain.exception.PriceNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * Traduce las excepciones a Problem Details (RFC 9457). Los errores de binding y
  * validación (400) ya los emite la clase base con ese formato.
  */
+@Slf4j
 @RestControllerAdvice
 class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -37,6 +39,7 @@ class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     ProblemDetail onUnexpected(Exception ex) {
+        log.error(INTERNAL_ERROR_DETAIL, ex);
         ProblemDetail problem =
                 ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_ERROR_DETAIL);
         problem.setTitle(INTERNAL_ERROR_TITLE);
